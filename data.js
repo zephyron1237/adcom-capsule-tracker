c93655b66bc9c9598f1bcb5f3785834be5348db0
zephyron1237 <princessbowl@gmail.com>
2020-04-27 16:09:41 -0700

    Added 25th plastic to event cycle
    Similar bug to motherland, there's an "extra" capsule at the end of the event cycle which is identical to the first one.

--- data.js
+++ data.js
@@ -13,5 +13,14 @@ for (let replacementIndex in MAIN_DATA_REPLACEMENTS) {
 
 const BASE_EVENT_GAME_DATA = ["plastic","plastic","armored","plastic","plastic","plastic","armored","plastic","plastic","plastic","armored","plastic","plastic","plastic","armored","plastic","plastic","plastic","armored","plastic","plastic","plastic","armored","plastic"];
 
+// Inexplicably, even though the Balance.json is exactly above, the game actually has an extra plastic in spot #25.
+const BASE_EVENT_DATA_REPLACEMENTS = { 
+  24: "plastic"
+};
+
+for (let replacementIndex in BASE_EVENT_DATA_REPLACEMENTS) {
+  BASE_EVENT_GAME_DATA[replacementIndex] = BASE_EVENT_DATA_REPLACEMENTS[replacementIndex];
+}
+
 // The first capsule in an event is a scripted plastic.  So we just move the plastic at the end to the beginning to fix the loop.
 const EVENT_GAME_DATA = ["plastic", ...BASE_EVENT_GAME_DATA.slice(0, -1)]
